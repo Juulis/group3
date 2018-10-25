5b1cbcd7fa8e46ee13dbe6122bd568ab912cabd6
mohammed <mohammedalmahfoodh@yahoo.com>
2018-10-26 01:07:31 +0200

    adding dependencies to project create test folder create DeskTest

--- src/models/Deck.java
+++ src/models/Deck.java
@@ -1,4 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Deck {
+    private Card card;
+    private ArrayList<Card>cards;
+
+    public Deck() {
+        cards=new ArrayList<>();
+    }
+    public void createFullDeck(){
+
+
+    }
 }
---
+++ test/models/DeckTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+
+class DeckTest {
+
+    @Test
+    void testCreateFullDeck() {
+
+   
+
+    }
+}
