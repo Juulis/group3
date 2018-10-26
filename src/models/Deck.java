401e492a43c2b8ff283049b6c6e4b4168df46d4a
Juulis <danny.p@live.se>
2018-10-26 10:08:37 +0200

    setup dependencies for mockito, cleaned up dev branch

--- src/models/Deck.java
+++ src/models/Deck.java
@@ -3,14 +3,4 @@ package models;
 import java.util.ArrayList;
 
 public class Deck {
-    private Card card;
-    private ArrayList<Card>cards;
-
-    public Deck() {
-        cards=new ArrayList<>();
-    }
-    public void createFullDeck(){
-
-
-    }
 }
