4aebbd1b313b3caa9db1f6d89aa3394d63e7f201
Tobias Ask <akas@live.se>
2018-10-27 01:39:16 +0200

    Test created for Card class

--- src/models/Card.java
+++ src/models/Card.java
@@ -1,4 +1,35 @@
 package models;
 
+import java.util.Random;
+
 public class Card {
+    private int hp;
+
+    public Card() {
+        this.hp = randomizeHp();
+    }
+
+    /**
+     * Determines hp of card
+     *
+     * @return random nr between 1-20
+     */
+    public int randomizeHp() {
+        return 0;
+    }
+
+
+    public int getHp() {
+        return hp;
+    }
+
+    /**
+     * When a card gets attacked, remove the amount of attack from the card
+     *
+     * @param amount of attack
+     * @return healthPoints after attack
+     */
+    public int removeHp(int hpToRemove) {
+        return 0;
+    }
 }
