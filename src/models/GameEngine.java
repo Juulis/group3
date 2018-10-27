6ec7f6a82bb9aef37a77991bf71bd0905267f29b
LidiaT <tothlidia80@gmail.com>
2018-10-27 12:16:35 +0200

    test for initPlayer method

--- src/models/GameEngine.java
+++ src/models/GameEngine.java
@@ -1,4 +1,37 @@
 package models;
 
+import java.util.ArrayList;
+
 public class GameEngine {
+
+    private Player p1, p2;
+    private ArrayList<Card> gameCards;
+
+    public void setP1(Player p){
+        this.p1=p;
+    }
+
+    public void setP2(Player p){
+        this.p2=p;
+    }
+
+    public void setGameCards(ArrayList<Card> gameCards){
+        this.gameCards=gameCards;
+    }
+
+    public Player getP1(){
+        return p1;
+    }
+
+    public Player getP2(){
+        return p2;
+    }
+
+    /**
+     * creates two player objects and
+     * sets the players decks and cards in hands
+     */
+    public void initPlayer(){
+
+    }
 }
--- src/models/Player.java
+++ src/models/Player.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    public void setCurrentDeck(ArrayList<Card> gameCards){
+
+    }
+
+    public void setPlayerHand(){
+
+    }
 }
