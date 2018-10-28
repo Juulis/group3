b94aa19af30e08c05235f216da1ff0c45cb6d516
MikaelFeher <mikael.feher@gmail.com>
2018-10-28 14:34:46 +0100

    Adds player class with initial health = 10

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Player {
+    private int health;
+
+    public Player() {
+        this.health = 10;
+    }
+
+    public int getHealth() {
+        return health;
+    }
 }
