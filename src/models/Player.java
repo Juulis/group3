41f86406305240269bebd9f6d3d7f2a1f327f4b0
LidiaT <tothlidia80@gmail.com>
2018-10-28 12:20:41 +0100

    test for pickupCard method

--- src/models/Player.java
+++ src/models/Player.java
@@ -4,10 +4,27 @@ import java.util.ArrayList;
 
 public class Player {
 
+    private ArrayList<Card> currentDeck=new ArrayList<>();
+    private ArrayList<Card> playerHand=new ArrayList<>();
+
     public void setCurrentDeck(ArrayList<Card> gameCards){
 
     }
 
+    public ArrayList<Card> getCurrentDeck(){
+
+        return currentDeck;
+    }
+
+    public ArrayList<Card> getPlayerHand(){
+
+        return playerHand;
+    }
+
+    /**
+     * takes one card from players deck and
+     * puts it in players hand
+     */
     public void pickupCard(){
 
     }
