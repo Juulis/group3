cb4d6fdf557fa8e4aab43bc22357792334efedba
Tobias Ask <akas@live.se>
2018-10-29 14:12:36 +0100

    NEW STRUCTURE WITH MAVEN AND JAVAFX

---
+++ src/main/java/models/Deck.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Deck {
+
+    private Card card;
+    private int getTotalCards;
+    private ArrayList<Card> cards;
+    private  int totalCards;
+
+    public Deck() {
+        cards=new ArrayList<Card>();
+        totalCards=50;
+    }
+
+    /**
+     *      create array of deck cards with 50 cards
+     */
+    public  void createFullDeck(){
+        for (int i=0;i<totalCards;i++){
+            cards.add(new Card());
+        }
+    }
+    /**
+     *
+     * @return  Deck Array of cards
+     */
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+}
---
+++ test/java/CardTest.java
@@ -0,0 +1,23 @@
+import models.*;
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    Card card = new Card();
+
+    @RepeatedTest(1000)
+    void randomizeHealthPoint() {
+        assertThat(card.randomizeHp()).isBetween(1, 20);
+    }
+
+    @RepeatedTest(1000)
+    void removeHealthPoints() {
+        int currentHp = card.getHp();
+        int hpToRemove = 10;
+        card.removeHp(hpToRemove);
+        int expected = currentHp - hpToRemove;
+        assertEquals(expected, card.getHp());
+    }
+}
