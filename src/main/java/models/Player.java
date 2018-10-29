5246e25d26ce66882c42c24d08029149fdfed8b9
Tobias Ask <akas@live.se>
2018-10-29 11:06:54 +0100

    Added maven as framework and added all dependecies to pom file

---
+++ src/main/java/models/Player.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Player {
+    private int health;
+    private ArrayList<Card> currentDeck=new ArrayList<Card>();
+    private ArrayList<Card> playerHand=new ArrayList<Card>();
+
+    public Player() {
+        this.health = 10;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setCurrentDeck(ArrayList<Card> gameCards){
+
+    }
+
+    public ArrayList<Card> getCurrentDeck(){
+
+        return currentDeck;
+    }
+
+    public ArrayList<Card> getPlayerHand(){
+
+        return playerHand;
+    }
+
+    /**
+     * takes one card from players deck and
+     * puts it in players hand
+     */
+    public void pickupCard(){
+
+        int index=currentDeck.size()-1;
+        Card card=currentDeck.remove(index);
+        playerHand.add(card);
+    }
+}
