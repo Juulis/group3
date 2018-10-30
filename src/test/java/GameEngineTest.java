131358430b8b3074c2c82ce856eff78a182b5b2f
Rickard Lundquist <rickard-lundquist@hotmail.com>
2018-10-30 14:19:47 +0100

    Tests to determine winner if players gameCards and HP is empty or not

--- src/test/java/GameEngineTest.java
+++ src/test/java/GameEngineTest.java
@@ -1,5 +1,6 @@
 
 import models.*;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -8,7 +9,11 @@ import org.mockito.junit.jupiter.*;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -21,6 +26,12 @@ class GameEngineTest {
     Player p1Mock, p2Mock;
     @Mock
     ArrayList<Card> gameCardsMock;
+
+    @Spy
+    ArrayList<Card> gameCardsSpy = spy(new ArrayList<Card>());
+
+    @Spy
+    Player players = spy(new Player());
 
     @BeforeEach
     void setUp() {
@@ -41,5 +52,34 @@ class GameEngineTest {
         verify(p2Mock, times(1)).setCurrentDeck(gameCardsMock);
         verify(p1Mock, times(5)).pickupCard();
         verify(p2Mock, times(5)).pickupCard();
+    }
+
+    @DisplayName("If player deck is null")
+    @Test
+    void checkDeckSizeIsNull() {
+        assertThat(gameCardsSpy.size(),is(equalTo(0)));
+        assertTrue(gameCardsSpy.isEmpty());
+    }
+
+    @DisplayName("If player deck is not null")
+    @Test
+    void checkDeckSizeNotNull() {
+        gameCardsSpy.add(new Card());
+        assertThat(gameCardsSpy.size(),is(equalTo(1)));
+        assertNotNull(gameCardsSpy);
+    }
+
+    @DisplayName("If player Hp is null")
+    @Test
+    void checkPlayerHpIsNull() {
+        assertNotNull(players);
+        assertThat(players.getHealth(),is(equalTo(10)));
+    }
+
+    @DisplayName("If player Hp is not null")
+    @Test
+    void checkPlayerHpNotNull() {
+        assertThat(players.getHealth(),is(equalTo(10)));
+        assertEquals(10,players.getHealth());
     }
 }
