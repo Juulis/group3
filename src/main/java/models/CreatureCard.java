ee8e3373bb21859474b6e12aec2a724f99f928eb
LidiaT <tothlidia80@gmail.com>
2018-11-07 20:41:42 +0100

    deleted attack atribute

--- src/main/java/models/CreatureCard.java
+++ src/main/java/models/CreatureCard.java
@@ -3,26 +3,20 @@ package models;
 public class CreatureCard extends Card {
 
     private int hp;
-    private int attack;
     private int defence;
     private int power;
+    private boolean tapped;
 
-    public CreatureCard(int hp, int attack, int defence, int power){
+    public CreatureCard(String name, int energy, int attack, String specialAttack, int hp, int defence, int power){
 
-        super();
+        super(name, energy, attack, specialAttack);
         this.hp = hp;
-        this.attack = attack;
         this.defence = defence;
         this.power = power;
     }
 
     public int getHp() {
         return hp;
-    }
-
-
-    public int getAttack() {
-        return attack;
     }
 
     public int getDefence() {
@@ -36,5 +30,17 @@ public class CreatureCard extends Card {
     public void removeHp( int dmg){
 
         this.hp -= dmg;
+    }
+
+    public boolean isTapped() {
+        return tapped;
+    }
+
+    public void tap(){
+        this.tapped = true;
+    }
+
+    public void unTap(){
+        this.tapped = false;
     }
 }
