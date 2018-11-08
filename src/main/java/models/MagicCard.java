94056e0c5be5453679bda13c100e79a295b3befa
LidiaT <tothlidia80@gmail.com>
2018-11-08 13:32:33 +0100

    added constructor in MagicCard

--- src/main/java/models/MagicCard.java
+++ src/main/java/models/MagicCard.java
@@ -1,5 +1,9 @@
 package models;
 
 public class MagicCard extends Card{
+
+    public MagicCard(int attack, int cardEnergy, String cardName, String specialAttack){
+        super(attack, cardEnergy, cardName, specialAttack);
+    }
 //To seperate CreatureCard from MagicCard
 }
