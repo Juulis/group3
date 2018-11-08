51a1ab3d2db97ce9028998e197c737802ca56033
Fatlum <fatlum93@gmail.com>
2018-11-08 08:59:42 +0100

    Made card class into a superclass

--- src/test/models/CardTest.java
+++ src/test/models/CardTest.java
@@ -11,30 +11,5 @@ import static org.junit.jupiter.api.Assertions.*;
 class CardTest {
 
     Card card = new Card();
-    Random random;
-    @BeforeEach
-    void setUp() {
-      random=new Random();
-    }
-
-    @RepeatedTest(1000)
-    void randomizeHealthPoint() {
-        assertThat(card.randomizeHp()).isBetween(1, 7);
-    }
-
-    @Test
-    void removeHealthPoints() {
-        int currentHp = card.getHp();
-        int hpToRemove = 10;
-        card.removeHp(hpToRemove);
-        int expected = currentHp - hpToRemove;
-        assertEquals(expected, card.getHp());
-    }
-    @DisplayName("testing attack method returning values between 1-6 ")
-    @RepeatedTest(1000)
-    void testAttack() {
-
-        assertThat(card.attack()).isBetween(1, 6);
-
-    }
+    
 }
