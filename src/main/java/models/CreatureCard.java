4f0eaeb935d6a57cd7a523919100d1a34e1bcad2
mohammed <mohammedalmahfoodh@yahoo.com>
2018-11-10 23:13:49 +0100

    implement ignite attack method

--- src/main/java/models/CreatureCard.java
+++ src/main/java/models/CreatureCard.java
@@ -7,13 +7,14 @@ public class CreatureCard extends Card {
     private int power;
     private boolean tapped;
     private int playedOnRound;
-
+    private int ignRoundCounter;
     
     public CreatureCard(int attack, int cardEnergy, String cardName, String specialAttack, int hp, int defence, int power){
         super(attack, cardEnergy, cardName, specialAttack);
         this.hp = hp;
         this.defence = defence;
         this.power = power;
+        ignRoundCounter=0;
     }
 
     public int getHp() {
@@ -31,6 +32,17 @@ public class CreatureCard extends Card {
     public void removeHp( int dmg){
 
         this.hp -= dmg;
+    }
+    public void increaseIgnRoundCounter() {
+        ignRoundCounter+=1;
+    }
+
+    public int getIgnRoundCounter() {
+        return ignRoundCounter;
+    }
+
+    public void setIgnRoundCounter(int ignRoundCounter) {
+        this.ignRoundCounter = ignRoundCounter;
     }
 
     public boolean isTapped() {
