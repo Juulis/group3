c78144f952b84309e1565f176189da0d7eaad6ba
LidiaT <tothlidia80@gmail.com>
2018-11-25 10:16:50 +0100

    switched from highscoreModel and tableModel to Highscore

--- src/main/java/controllers/HighscoreController.java
+++ src/main/java/controllers/HighscoreController.java
@@ -7,7 +7,7 @@ import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.*;
 import javafx.scene.layout.*;
-import pojo.*;
+import models.Highscore;
 import java.io.*;
 import java.util.*;
 
@@ -16,11 +16,11 @@ public class HighscoreController {
     @FXML
     private AnchorPane highscorePane;
     @FXML
-    private TableView<TableModel> highscoreTableView;
+    private TableView<Highscore> highscoreTableView;
     @FXML
-    private TableColumn<TableModel, String> col_name;
+    private TableColumn<Highscore, String> col_name;
     @FXML
-    private TableColumn<TableModel, String> col_score;
+    private TableColumn<Highscore, String> col_score;
 
     public void initialize() throws FileNotFoundException {
         System.out.println("Changed scene to Highscore.fxml");
@@ -34,8 +34,8 @@ public class HighscoreController {
 
     public void getHighscore() throws FileNotFoundException {
         JsonReader reader = new JsonReader(new FileReader("src/main/java/json/Highscore.json"));
-        List<TableModel> highscore = Arrays.asList(new Gson().fromJson(reader, HighscoreModel[].class));
-        ObservableList<TableModel> list = FXCollections.observableArrayList(highscore);
+        List<Highscore> highscore = Arrays.asList(new Gson().fromJson(reader, Highscore[].class));
+        ObservableList<Highscore> list = FXCollections.observableArrayList(highscore);
         col_name.setCellValueFactory(new PropertyValueFactory<>("name"));
         col_score.setCellValueFactory(new PropertyValueFactory<>("score"));
         highscoreTableView.setItems(list);
